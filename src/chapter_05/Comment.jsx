01b9228dcbacf6cb6aed3569ec7c6a5dd83ad250
99hoyoung <eddies0429@naver.com>
2023-04-06 11:34:25 +0900

    0406

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -30,29 +30,24 @@ const styles = {
         color: "black",
         fontSize: 16,
     },
-  };
+};
 
-  function Comment(props) {
+function Comment(props) {
     return (
         <div style={styles.wrapper}>
             <div style={styles.imageContainer}>
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
-                    alt = "프로필 이미지"
+                    alt = "이미지"
                     style={styles.image}
-
+                    
                 />
             </div>
 
             <div style={styles.contentContainer}>
-                <span style={styles.nameText}>이호영</span>
-                <span style={styles.commentText}>
-                    내가 만든 첫 컴포넌트.
-                </span>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>{props.comment}</span>
             </div>
         </div>
-    )
+    );
 }
-
-
-export default Comment
