58d1c5161f75419946a6d80a2aee9935ec0f8806
99hoyoung <eddies0429@naver.com>
2023-04-06 11:32:48 +0900

    0406

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,30 +1,29 @@
 import React from "react";
-import Comment from "./Comment"
+import Comment from "./Comment";
 
 const comments = [
-    {
-        name: "이호영",
-        comment: "안녕하세요, 이호영입니다.",
-    },
-    {
-        name: "이호영1",
-        comment: "안녕하세요, 이호영1입니다.",
-    },
-    {
-        name: "이호영2",
-        comment: "안녕하세요, 이호영2입니다.",
-    },
+  {
+    name: "이인제",
+    comment: "안녕하세요, 소플입니다.",
+  },
+  {
+    name: "유재석",
+    comment: "리액트 재미있어요~!",
+  },
+  {
+    name: "강민경",
+    comment: "저도 리액트 배워보고 싶어요!!",
+  },
 ];
 
 function CommentList(props) {
-    return (
-        <div>
-            {comments.map((comment) => {
-                return (
-                    <Comment name={comment.name} comment={comment.comment} />
-                );
-            })}
-        </div>
-    );
+  return (
+    <div>
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
+    </div>
+  );
 }
-export default CommentList
+
+export default CommentList;
